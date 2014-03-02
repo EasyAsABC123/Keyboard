531ac5295b9f3cf14e2f99b95f89b8c0c9df69b9
Justin Schuhmann <justin.schuhmann@e-hps.com>
2014-03-02 05:00:44 -0500

    Example added
    Notepad standard only accepts foreground key press use notepad++ if you want to demonstrate background key presses

---
+++ KeyboardDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Keyboard;
+
+namespace KeyboardDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var p = new Process {StartInfo = {FileName = "notepad.exe"}};
+
+			p.Start();
+
+			try
+			{
+				List<Key> keys = "hello world".Select(c => new Key(c)).ToList();
+				var procId = p.Id;
+				Console.WriteLine("ID: " + procId);
+				Console.WriteLine("Sending background keypresses to write \"hello world\"");
+				p.WaitForInputIdle();
+				foreach (var key in keys)
+				{
+					key.PressForeground(p.MainWindowHandle);
+				}
+
+			}
+			catch (InvalidOperationException)
+			{
+			}
+		}
+	}
+}
